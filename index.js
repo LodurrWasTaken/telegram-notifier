bd380232e3fa9ba7923a9ee07288279a4e1dad54
LodurrWasTaken <evvmoroz@gmail.com>
2018-09-27 15:48:53 +0300

    parse cmd line arguments

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const https = require('https');
 const fs = require('fs');
 
-const token = '';
-const channel = '@';
-const file = '';
-const bufferSize = 2000;
+let config = {
+    token: '',
+    channel: '@',
+    file: '',
+    bufferSize: 2000
+};
 
-fs.watchFile(file, (curr, prev) => {
-    fs.open(file, 'r', (err, fd) => {
+for (let i = 0, arr = process.argv.slice(2), l = arr.length; i < l; i++) {
+    let spl = arr[i].split('=');
+    config[spl[0]] = spl[1];
+}
+
+fs.watchFile(config.file, (curr, prev) => {
+    fs.open(config.file, 'r', (err, fd) => {
         if (err) throw err;
 
-        let buffer = Buffer.alloc(bufferSize);
+        let buffer = Buffer.alloc(+config.bufferSize);
         fs.read(fd, buffer, 0, curr.size - prev.size, prev.size, (err, num) => {
             let msg = buffer.toString('utf8', 0, num);
             https.get(
-                `https://api.telegram.org/bot${token}/sendMessage?chat_id=${channel}&text=${msg}`
+                `https://api.telegram.org/bot${
+                    config.token
+                }/sendMessage?chat_id=${config.channel}&text=${msg}`
             );
         });
     });
